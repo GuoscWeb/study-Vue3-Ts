8e36d3f88aa9d5b6a8ef5d1a570a87b6a3bd7130
diligentG <447781682@qq.com>
2022-03-04 00:34:24 +0800

    webpack基本使用

---
+++ 08-LEARN-WEBPACK/01-basic-webpack/src/index.js
@@ -0,0 +1,5 @@
+import { sum } from './js/math.js'
+const { formatDecimal } = require('./js/format.js')
+
+console.log(sum(1, 2));
+console.log(formatDecimal(999.456789, 2));
---
+++ 08-LEARN-WEBPACK/01-basic-webpack/src/js/math.js
@@ -0,0 +1,8 @@
+// ES module 的导出
+export const sum = (a, b) => {
+  return a + b
+}
+
+// export function sum() {
+  
+// }
